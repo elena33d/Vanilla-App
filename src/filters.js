c7f36b8cc75fe5cd32ef1be54c3cb8336ef08573
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-01-29 00:57:28 +0100

    Update filters.js

--- src/filters.js
+++ src/filters.js
@@ -1,8 +1,1 @@
-const filterButtons = document.querySelectorAll('.filterBtn');
 
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    filterButtons.forEach(btn => btn.classList.remove('isActive'));
-    button.classList.add('isActive');
-  });
-});

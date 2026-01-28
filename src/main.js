7bc7005c40d1523c248910951a91deb4ac60b6dd
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-01-29 00:38:39 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import headerTemplate from './partials/header.html';
+import heroTemplate from './partials/hero.html';
 
 const header = document.querySelector('.header');
+const hero = document.querySelector('.hero');
+
 header.innerHTML = headerTemplate;
+hero.innerHTML = heroTemplate;

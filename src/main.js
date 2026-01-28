f70977de206c4e1aef45396d8dfa904e24cc21e9
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-01-29 00:44:17 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import headerTemplate from './partials/header.html';
 import heroTemplate from './partials/hero.html';
 import filtersTemplate from './partials/filters.html';
+import './js/filters';
 
 const filters = document.querySelector('.filters');
 filters.innerHTML = filtersTemplate;
@@ -10,4 +11,5 @@ const hero = document.querySelector('.hero');
 
 header.innerHTML = headerTemplate;
 hero.innerHTML = heroTemplate;
+
 

b73dd36a2dab0cd3ea8df85bcb84d45aee1f4ad1
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-01-29 00:40:47 +0100

    Add filters template to main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import headerTemplate from './partials/header.html';
 import heroTemplate from './partials/hero.html';
+import filtersTemplate from './partials/filters.html';
+
+const filters = document.querySelector('.filters');
+filters.innerHTML = filtersTemplate;
 
 const header = document.querySelector('.header');
 const hero = document.querySelector('.hero');
 
 header.innerHTML = headerTemplate;
 hero.innerHTML = heroTemplate;
+
